a9b48a69ecca214409cb854525b79ba780c0b3e5
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-01-01 08:32:44 +0100

    footer and social media thubnails updated

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,10 @@ import React from 'react';
 import '../styles/footer.css';
 
 function footer(){
+     let d = new Date();
      return(
           <div className='footer'>
-               <span className='footer-text'>&copy; Copyright 2021 Hanif Adedotun. All rights reserved.</span>
+               <span className='footer-text'>&copy; Copyright {d.getFullYear()} Hanif Adedotun. All rights reserved. Made with Passion.</span>
           </div>
      )
 }
