ffc2f1f1cc2512ec12c45777df91b0011f828640
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-03-13 18:20:58 +0100

    added kaggle certification

--- src/components/certifications.js
+++ src/components/certifications.js
@@ -8,6 +8,7 @@ import {Grid} from '@mui/material';
 import Footer from './footer';
 
 // Certificates
+import MLIntro from '../images/certificates/ML.png';
 import Excel2013 from '../images/certificates/excel2013.jpg';
 import PP2016 from '../images/certificates/powerpoint2016.jpg';
 import AIUdemy from '../images/certificates/ai-udemy.png';
@@ -17,41 +18,48 @@ import BusF from '../images/certificates/BF.jpg';
 
 const certificates = {
      1:{
+          'name':'Kaggle: Intro to Machine Learning',
+          'org': 'Kaggle',
+          'img': MLIntro,
+          'year': '2022',
+          'link': 'https://www.kaggle.com/learn/certification/hanifai/intro-to-machine-learning '
+     },
+     2:{
           'name':'Microsoft Office Specialist: Microsoft Office Excel 2013',
           'org': 'Microsoft',
           'img': Excel2013,
           'year': '2019',
           'link': 'https://portal.certiport.com/Portal/Pages/PrintTranscriptInfo.aspx?action=Cert&id=192&cvid=XRSAFO7FalspSbH6Om0qqA=='
      },
-     2:{
+     3:{
           'name':'Microsoft Office Specialist: Microsoft Office PowerPoint 2016',
           'org': 'Microsoft',
           'img': PP2016,
           'year': '2018',
           'link': 'https://portal.certiport.com/Portal/Pages/PrintTranscriptInfo.aspx?action=Cert&id=252&cvid=iqjugrOUSZAauzZFR7BYSQ=='
      },
-     3:{
+     4:{
           'name':'Artificial Intelligence Intro',
           'org': 'IBM',
           'img': AIUdemy,
           'year': '2021',
           'link': 'https://www.credly.com/badges/a964634c-5205-4059-929b-333668281ed1/public_url'
      },
-     4:{
+     5:{
           'name':'Google Solution Challange 2021',
           'org': 'Google Developer student challange',
           'img': Solution,
           'year': '2021',
           'link': 'https://drive.google.com/file/d/1nx-qA9ZnbyR15-qayYo9RktJJxw0zjoP/view?usp=sharing'
      },
-     5:{
+     6:{
           'name': 'Teamwork and Team Building',
           'org': 'Nigeria Association of Computing Students (NACOS)',
           'img': TTB,
           'year': '2021',
           'link': 'https://drive.google.com/file/d/1LVdvIoBq9vt7I9GC120NqRm-ux2TUuZl/view?usp=sharing'
      },
-     6:{
+     7:{
           'name': 'Business Foundation',
           'org': 'Nigeria Association of Computing Students (NACOS)',
           'img': BusF,
