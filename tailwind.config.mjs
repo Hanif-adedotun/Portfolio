19027150f980bb5e8dd43d53df2f5d7147593ecd
Hanif <hanif.adedotun@gmail.com>
2025-12-29 22:01:03 +0100

    v2 portfolio added

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+      colors: {
+        contrast: '#1DCFF6',
+      },
+    },
+  },
+  plugins: [],
+}
